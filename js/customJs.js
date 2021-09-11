3bba267069dcf34b15035007d539f393fb108178
iftakharahmedchowdhury <iftakharahmedchowdhuryimacc@gmail.com>
2021-09-11 11:01:30 +0600

    first commit

---
+++ js/customJs.js
@@ -0,0 +1,205 @@
+const printerPart=()=>{
+    // console.log("I am printer")
+    const mainDiv= document.getElementById('printersOrPc');
+    mainDiv.innerHTML=`
+
+    <div class="container">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                               
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                          
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                    <!-- 2 -->
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/01Printer1.jpg" class="card-img-top  w-100 w-75" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                   
+                    
+                </div>
+            </div>
+    
+    `;
+}
+printerPart();
+
+const PCSPart=()=>{
+    // console.log("I am printer")
+    const mainDiv= document.getElementById('printersOrPc');
+    mainDiv.innerHTML="";
+    mainDiv.innerHTML=`
+      
+    <div class="container">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                               
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                           
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                          
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                   
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class=" h-100">
+                            <img src="images/devices/laptop.webp" class="card-img-top  w-100 w-75" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                              
+                            </div>
+                            <div>
+                                <button type="button" class="btn btn-primary">Primary</button>
+                                <a href="">shop</a>
+                            </div>
+                         
+                        </div>
+                    </div>
+                   
+                    
+                </div>
+            </div>
+    
+    `;
+}
+
+
+////search
+
